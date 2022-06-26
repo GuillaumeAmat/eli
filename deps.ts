bd8582f8f42dbd6a3c740a7df48dd15e368ea5e4
Guillaume AMAT <guillaume@amat.io>
2022-06-27 00:49:51 +0200

    feat: fuzzy search test

--- deps.ts
+++ deps.ts
@@ -1,2 +1,7 @@
-export { Command } from "cliffy";
-export { createMachine, interpret } from "xstate";
+export { Command } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
+export {
+  Select,
+  prompt,
+} from "https://deno.land/x/cliffy@v0.24.2/prompt/mod.ts";
+
+export { createMachine, interpret } from "https://esm.sh/xstate@4.32.1";
--- main.ts
+++ main.ts
@@ -1,9 +1,26 @@
-import { Command } from "./deps.ts";
+import { Command, Select, prompt } from "./deps.ts";
 import { eli } from "./eli.ts";
 
 await new Command()
   .name("eli")
   .description("Eli, your devoted assistant")
   .version("v0.1.0")
-  .action(() => eli.start())
+  .action(async () => {
+    eli.start();
+
+    await prompt([
+      {
+        name: "entrypoint",
+        message: "",
+        search: true,
+        type: Select,
+        options: [
+          { name: "Do this", value: "do-this" },
+          { name: "Do that", value: "do-that" },
+          { name: "Or something like that", value: "or-something-like-that" },
+        ],
+        after: (_, next) => next("entrypoint"),
+      },
+    ]);
+  })
   .parse(Deno.args);
