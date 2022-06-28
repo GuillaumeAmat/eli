2107770e9e7593a44df2ee028ae85850f9212d26
Guillaume AMAT <guillaume.amat@backmarket.com>
2022-06-28 20:44:49 +0200

    refactor: use import.meta.main

--- main.ts
+++ main.ts
@@ -1,26 +1,32 @@
 import { Command, Select, prompt } from "./deps.ts";
 import { eli } from "./eli.ts";
 
-await new Command()
-  .name("eli")
-  .description("Eli, your devoted assistant")
-  .version("v0.1.0")
-  .action(async () => {
-    eli.start();
+async function main() {
+  await new Command()
+    .name("eli")
+    .description("Eli, your devoted assistant")
+    .version("v0.1.0")
+    .action(async () => {
+      eli.start();
 
-    await prompt([
-      {
-        name: "entrypoint",
-        message: "",
-        search: true,
-        type: Select,
-        options: [
-          { name: "Do this", value: "do-this" },
-          { name: "Do that", value: "do-that" },
-          { name: "Or something like that", value: "or-something-like-that" },
-        ],
-        after: (_, next) => next("entrypoint"),
-      },
-    ]);
-  })
-  .parse(Deno.args);
+      await prompt([
+        {
+          name: "entrypoint",
+          message: "",
+          search: true,
+          type: Select,
+          options: [
+            { name: "Do this", value: "do-this" },
+            { name: "Do that", value: "do-that" },
+            { name: "Or something like that", value: "or-something-like-that" },
+          ],
+          after: (_, next) => next("entrypoint"),
+        },
+      ]);
+    })
+    .parse(Deno.args);
+}
+
+if (import.meta.main) {
+  main();
+}
